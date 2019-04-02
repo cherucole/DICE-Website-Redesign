d706ecb47ea2e7d0df2bcd30f3ad1f4e6e95c091
cherucole <cheruyotcheruyot@gmail.com>
2019-04-02 17:17:00 +0300

    navbar items set

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ function classToggle() {
 
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
-document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
