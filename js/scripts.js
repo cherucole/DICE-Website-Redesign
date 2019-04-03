49788d362ba8453414480cb0f23fc3c433bde6fe
cherucole <cheruyotcheruyot@gmail.com>
2019-04-03 14:04:46 +0300

    added sidescrolling

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ function classToggle() {
 }
 
 document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+
+// polyfill
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+var speed = 5000;
+(function currencySlide() {
+    var currencyPairWidth = $('.slideItem:first-child').outerWidth();
+    $(".slideContainer").animate({
+        marginLeft: -currencyPairWidth
+    }, speed, 'linear', function () {
+        $(this).css({
+            marginLeft: 0
+        }).find("li:last").after($(this).find("li:first"));
+    });
+    requestAnimationFrame(currencySlide);
+})();
